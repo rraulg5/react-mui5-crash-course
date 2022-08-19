a699a99e5301ebbdb7c9d908343979ea62700b1e
Raul Garcia <rraulg5@gmail.com>
2022-08-19 14:29:14 -0500

    Custom Themes with Providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import './App.css';
+import SearchAppBar from './components/AppBar';
 import TourCard from './components/TourCard';
+import cities from './data.json';
 
 function App() {
   return (
-    <Container maxWidth="lg">
-      <Grid container spacing={3}>
-        <TourCard imgRandom={1} />
-        <TourCard imgRandom={2} />
-        <TourCard imgRandom={3} />
-        <TourCard imgRandom={4} />
-      </Grid>
-    </Container>
+    <>
+      <SearchAppBar />
+      <Container maxWidth="lg" sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <Grid key={city.id}>
+            <Typography
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={3}>
+              {city.tours.map((tour) => (
+                <TourCard tour={tour} key={tour.id} />
+              ))}
+            </Grid>
+          </Grid>
+        ))}
+      </Container>
+    </>
   );
 }
 
--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,40 +1,74 @@
 import { AccessTime } from '@mui/icons-material';
-import { Box, Grid, Paper, Rating, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
 
-export default function TourCard({ imgRandom = 1 }) {
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: 'body2',
+          },
+          style: {
+            fontSize: 14,
+          },
+        },
+        {
+          props: {
+            variant: 'body3',
+          },
+          style: {
+            fontSize: 12,
+          },
+        },
+      ],
+    },
+  },
+});
+
+export default function TourCard({ tour }) {
   return (
     <Grid item xs={3}>
-      <Paper elevation={5}>
-        <img
-          src={`https://picsum.photos/800/200?random=${imgRandom}`}
-          alt={`img-${imgRandom}`}
-        />
-        <Box padding={1}>
-          <Typography variant="h5" component="h2">
-            Immerse into the Falls
-          </Typography>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <AccessTime sx={{ width: 14 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={5}>
+          <img src={tour.image} alt={tour.name} />
+          <Box padding={1}>
+            <Typography variant="h6" component="h2">
+              {tour.name}
             </Typography>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <AccessTime sx={{ width: 14 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.duration} hours
+              </Typography>
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center' }} marginTop={2}>
+              <Rating
+                value={tour.rating}
+                precision={0.5}
+                size="small"
+                readOnly
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                {tour.rating}
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={2}>
+                ({tour.numberOfReviews} reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={1.5}>
+                From ${tour.price}
+              </Typography>
+            </Box>
           </Box>
-          <Box sx={{ display: 'flex', alignItems: 'center' }} marginTop={2}>
-            <Rating value={3.5} precision={0.5} size="small" readOnly />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body3" component="p" marginLeft={0.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={1.5}>
-              From $999
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 }
